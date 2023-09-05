b6c6d9d1b86c5352314e274036d19e152fd58571
chiragjain009 <70319087+chiragjain009@users.noreply.github.com>
2023-09-05 16:02:07 +0530

    Time: 1 ms (100%), Space: 44.8 MB (59.78%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,48 +1,70 @@
 class Solution {
+    
     public int[] sortedSquares(int[] nums) {
-        int indexTillNeg=0;
-        ArrayList<Integer> arrlist = new ArrayList<Integer>(nums.length);
-        for(int index=0;index<nums.length;index++){
-            if(nums[index]>=0){arrlist.add(nums[index]);}
-            else indexTillNeg=index;
-        }
-
-        int posIndex=0;
-        boolean fullNeg=false;
-        if(indexTillNeg==nums.length-1)fullNeg=true;
-        while(indexTillNeg>=0 && posIndex<nums.length && arrlist.size()!=nums.length){
-            if(arrlist.size()-1==posIndex){
-                int val=nums[indexTillNeg]*-1;
-                if(val<=arrlist.get(posIndex)){
-                    arrlist.add(posIndex,val);
-                    indexTillNeg--;
-                }
-                else arrlist.add(nums[indexTillNeg--]);posIndex++;
+        int left = 0;
+        int right = nums.length -1;
+        int index = nums.length- 1;
+        int result[] = new int [nums.length];
+        while(left<=right)
+        {
+            if(Math.abs(nums[left])>Math.abs(nums[right]))
+            {
+                result[index] = nums[left] * nums[left];
+                left++;
             }
-            else if(fullNeg){
-                int val=nums[indexTillNeg]*-1;
-                System.out.println(val);
-                arrlist.add(posIndex,val);
-                indexTillNeg--;posIndex++;
-            }else{
-                int val=nums[indexTillNeg]*-1;
-                if(val<=arrlist.get(posIndex)){
-                    arrlist.add(posIndex,val);
-                    indexTillNeg--;
-                }
-                else if(val>=arrlist.get(posIndex) && val<=arrlist.get(posIndex+1)){
-                    arrlist.add(posIndex+1,val);
-                    indexTillNeg--;posIndex++;
-                }else{
-                    posIndex++;
-                }
+            else
+            {
+                result[index] = nums[right] * nums[right];
+                right--;
             }
+            index--;
         }
-
-        for(int index=0;index<nums.length;index++){
-            nums[index]=arrlist.get(index) * arrlist.get(index);
-        }
-        return nums;
-
+        return result;
     }
+    // public int[] sortedSquares(int[] nums) {
+    //     int indexTillNeg=0;
+    //     ArrayList<Integer> arrlist = new ArrayList<Integer>(nums.length);
+    //     for(int index=0;index<nums.length;index++){
+    //         if(nums[index]>=0){arrlist.add(nums[index]);}
+    //         else indexTillNeg=index;
+    //     }
+
+    //     int posIndex=0;
+    //     boolean fullNeg=false;
+    //     if(indexTillNeg==nums.length-1)fullNeg=true;
+    //     while(indexTillNeg>=0 && posIndex<nums.length && arrlist.size()!=nums.length){
+    //         if(arrlist.size()-1==posIndex){
+    //             int val=nums[indexTillNeg]*-1;
+    //             if(val<=arrlist.get(posIndex)){
+    //                 arrlist.add(posIndex,val);
+    //                 indexTillNeg--;
+    //             }
+    //             else arrlist.add(nums[indexTillNeg--]);posIndex++;
+    //         }
+    //         else if(fullNeg){
+    //             int val=nums[indexTillNeg]*-1;
+    //             System.out.println(val);
+    //             arrlist.add(posIndex,val);
+    //             indexTillNeg--;posIndex++;
+    //         }else{
+    //             int val=nums[indexTillNeg]*-1;
+    //             if(val<=arrlist.get(posIndex)){
+    //                 arrlist.add(posIndex,val);
+    //                 indexTillNeg--;
+    //             }
+    //             else if(val>=arrlist.get(posIndex) && val<=arrlist.get(posIndex+1)){
+    //                 arrlist.add(posIndex+1,val);
+    //                 indexTillNeg--;posIndex++;
+    //             }else{
+    //                 posIndex++;
+    //             }
+    //         }
+    //     }
+
+    //     for(int index=0;index<nums.length;index++){
+    //         nums[index]=arrlist.get(index) * arrlist.get(index);
+    //     }
+    //     return nums;
+
+    // }
 }
