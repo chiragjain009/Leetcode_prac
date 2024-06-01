c9d0e18e42436a4dd15132a139191e0c7f581999
chirag jain <70319087+chiragjain009@users.noreply.github.com>
2024-06-01 13:41:07 +0530

    Time: 65 ms (38.67%), Space: 45.8 MB (12.84%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -3,29 +3,24 @@ class Solution {
         if(word1.length() != word2.length()) return false;
 
         HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> ocuur = new HashMap<>();
         for(char c:word1.toCharArray())map1.put( c , map1.getOrDefault(c,0)+1 );
-        
+
+        HashMap<Integer,Integer> ocuur = new HashMap<>();
         for(Integer i:map1.values())ocuur.put( i , ocuur.getOrDefault(i,0)+1 );
 
         HashMap<Character,Integer> map2 = new HashMap<>();
         for(char c:word2.toCharArray())map2.put( c , map2.getOrDefault(c,0)+1 );
 
-        // for(char c: word2.toCharArray()){
-        //     if(!map1.containsKey(c))return false;
-        // }
-        System.out.println(ocuur);
         for(Character i:map2.keySet()){
+
             if(!map1.containsKey(i))return false;
+
             Integer val = map2.get(i);
             if(ocuur.containsKey(val) && ocuur.get(val)>0 ){
                 ocuur.put( val , ocuur.get(val)-1 );
             }else return false;
+            
         }
-        System.out.println(map1);
-        System.out.println(map2);
-        System.out.println(ocuur);
-
         return true;
     }
 }
